0b430f64fed8a6120151d7762df4db3d3aad1de9
gc <dev.gauravchauhan@gmail.com>
2025-02-11 10:31:49 +0530

    getting started with view

---
+++ composeApp/src/commonMain/kotlin/com/gc/docswallet/presentation/MainScreen.kt
@@ -0,0 +1,24 @@
+package com.gc.docswallet.presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+
+//
+// Created by Code For Android on 11/02/25.
+// Copyright (c) 2025 CFA. All rights reserved.
+//
+
+@Composable
+fun MainScreenView() {
+
+    Box(
+        modifier = Modifier.fillMaxSize().background(Color.Black)
+    ) {}
+
+
+}
---
+++ composeApp/src/commonMain/kotlin/com/gc/docswallet/utils/Util.kt
@@ -0,0 +1,17 @@
+package com.gc.docswallet.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import org.jetbrains.compose.resources.Font
+import docswalletinfo.composeapp.generated.resources.Res
+import docswalletinfo.composeapp.generated.resources.pacifico_Regular
+
+
+//
+// Created by Code For Android on 11/02/25.
+// Copyright (c) 2025 CFA. All rights reserved.
+//
+
+@Composable
+fun ChewyFontFamily() = FontFamily(Font(Res.font.pacifico_Regular))
