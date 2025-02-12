3397d8d7ddb6b8677b45aa9557274da62f464cfb
gc <dev.gauravchauhan@gmail.com>
2025-02-12 11:09:22 +0530

    getting started with view

--- composeApp/src/commonMain/kotlin/com/gc/docswallet/presentation/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/gc/docswallet/presentation/MainScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -68,6 +69,15 @@ fun MainScreenView() {
 
                 }
             }
+
+         Button(
+             onClick = { /*TODO*/ },
+             modifier = Modifier
+
+         )   {
+             Text("Download")
+
+         }
 
         }
 
