07bc61ebcde317040c0b0bceedd7cb2492d37a41
gc <dev.gauravchauhan@gmail.com>
2025-02-12 11:15:24 +0530

    getting started with view and adding button

---
+++ composeApp/src/androidMain/kotlin/com/gc/docswallet/presentation/common/HandleIntentButton.android.kt
@@ -0,0 +1,7 @@
+package com.gc.docswallet.presentation.common
+
+actual class HandleIntentButton {
+    actual fun openPlayStore() {
+    }
+
+}
---
+++ composeApp/src/commonMain/kotlin/com/gc/docswallet/presentation/common/HandleIntentButton.kt
@@ -0,0 +1,12 @@
+package com.gc.docswallet.presentation.common
+
+
+//
+// Created by Code For Android on 12/02/25.
+// Copyright (c) 2025 CFA. All rights reserved.
+//
+
+expect class HandleIntentButton {
+
+    fun openPlayStore()
+}
