0e64e769e86a892621ac9237f91226162513f947
gc <dev.gauravchauhan@gmail.com>
2025-02-13 09:52:45 +0530

    added koin and settings

---
+++ composeApp/src/commonMain/kotlin/com/gc/docswallet/di/KoinModule.kt
@@ -0,0 +1,21 @@
+package com.gc.docswallet.di
+
+import org.koin.core.KoinApplication
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+
+
+//
+// Created by Code For Android on 13/02/25.
+// Copyright (c) 2025 CFA. All rights reserved.
+//
+
+expect val targetModule: Module
+
+fun initKoin(KoinApplication: (KoinApplication.() -> Unit)? = null) {
+
+    startKoin {
+        KoinApplication?.invoke(this)
+        modules(targetModule)
+    }
+}
