7ebcfd0b47bf4a1862d044e38700a34949a9850b
gc <dev.gauravchauhan@gmail.com>
2025-02-17 11:03:19 +0530

    added koin and settings

--- composeApp/src/androidMain/kotlin/com/gc/docswallet/di/KoinModule.android.kt
+++ composeApp/src/androidMain/kotlin/com/gc/docswallet/di/KoinModule.android.kt
@@ -1,10 +1,13 @@
 package com.gc.docswallet.di
 
 import android.app.Activity
+import android.content.Context
+import com.gc.docswallet.presentation.MainViewModel
 import com.gc.docswallet.presentation.common.HandleIntentButton
 import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
+import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
 
 actual val targetModule: Module = module {
--- composeApp/src/androidMain/kotlin/com/gc/docswallet/presentation/common/HandleIntentButton.android.kt
+++ composeApp/src/androidMain/kotlin/com/gc/docswallet/presentation/common/HandleIntentButton.android.kt
@@ -7,19 +7,22 @@ import android.net.Uri
 
 actual class HandleIntentButton(val context: Context) {
 
+    val appContext = context.applicationContext
+
     actual fun openPlayStore(url: String) {
         require(url.isNotBlank()) { "Package name cannot be empty or blank" }
 
         try {
-            context.startActivity(createPlayStoreIntent("com.gc.docswallet"))
+            appContext.startActivity(createPlayStoreIntent("com.gc.docswallet"))
         } catch (e: ActivityNotFoundException) {
-            context.startActivity(createWebIntent("com.gc.docswallet"))
+            appContext.startActivity(createWebIntent("com.gc.docswallet"))
         }
     }
 
     private fun createPlayStoreIntent(packageName: String): Intent {
         return Intent(Intent.ACTION_VIEW).apply {
             data = Uri.parse("market://details?id=$packageName")
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
             setPackage("com.android.vending")
         }
     }
@@ -27,6 +30,7 @@ actual class HandleIntentButton(val context: Context) {
     private fun createWebIntent(packageName: String): Intent {
         return Intent(Intent.ACTION_VIEW).apply {
             data = Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
         }
     }
 
